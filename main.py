660cdb98ce5073284d64b8fb8179b31dbf7c8630
estuardo8u14 <estuaro8u14@gmail.com>
2022-12-19 16:56:23 -0600

    sfd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_cors import cross_origin
+from flask_cors import cross_origin, CORS
 
 #Import ccxt
 #Para futuro si queremos que ejecute ordenes automatizadas por binance
@@ -8,8 +8,11 @@ API_KEY = 'your_api_key'
 API_SECRET = 'your_secret_key' """
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def root():
     return "OK"
 
