f5ab765a3bf04c4014e3df4ced927d25a2fcf929
estuardo8u14 <estuaro8u14@gmail.com>
2022-12-19 16:13:15 -0600

    dev2

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def root():
     return "OK"
 
 @app.route("/alerta", methods=['POST'])
-
 def alerta():
 
     msg = request.json
@@ -35,6 +34,14 @@ def alerta():
         'msg': msg
     }
 
+@app.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Estuardo",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
 
 #https://dazapi.herokuapp.com/alerta
 
