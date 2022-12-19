009e78357e3673f4df5a07a4b5cb7342a55cea93
estuardo8u14 <estuaro8u14@gmail.com>
2022-12-19 14:18:00 -0600

    hreaded option to enable multiple instances for multiple user access support

--- main.py
+++ main.py
@@ -33,6 +33,11 @@ def alerta():
         'msg': msg
     }
 
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
+
+    
 #mensaje de tradingview
 """ {
 "time": {{time}},
