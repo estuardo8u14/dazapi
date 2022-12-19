a41613efcf3a952accd9919bd9de8edea548952f
estuardo8u14 <estuaro8u14@gmail.com>
2022-12-19 14:11:45 -0600

    arreglos

--- config.py
+++
@@ -1,5 +0,0 @@
-#para futuro si queremos que ejecute ordenes automatizadas por binance
-PASSWORD = 'your_password'
-API_KEY = 'your_api_key' 
-API_SECRET = 'your_secret_key'
-
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask, request
-#import ccxt
+#Import ccxt
+#Para futuro si queremos que ejecute ordenes automatizadas por binance
+""" PASSWORD = 'your_password'
+API_KEY = 'your_api_key' 
+API_SECRET = 'your_secret_key' """
+
+
 
 
 app = Flask(__name__)
