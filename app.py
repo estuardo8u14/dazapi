36e0769f832c8b8d20f6f2e8c7536f95dee8d308
estuardo8u14 <estuaro8u14@gmail.com>
2022-12-19 13:48:45 -0600

    pruebas1

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 from flask import Flask, request
+import ccxt
+import config
 
 app = Flask(__name__)
+
+
 
 @app.route("/alerta", methods=['POST'])
 
 def alerta():
 
     msg = request.json
-    asset = msg['ticker']
-    print(asset)
+    tiempo = msg['time']
+    exchange = msg['exchange']
+    ticker = msg['ticker']
+    texto = msg['texto']
+    print(tiempo)
+    print(exchange)
+    print(ticker)
+    print(texto)
+
+
 
     return {
-        'code': 'Success',
+        'code': 'Exitosa',
         'msg': msg
     }
 
+#mensaje de tradingview
+""" {
+"time": {{time}},
+"exchange":  {{exchange}},
+"ticker": {{ticker}}",
+"texto": "Alert! Short-Venta"
+} """
